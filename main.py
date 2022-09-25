8357902da8827d94e2f7d2646b0ba58424cb4e98
iveiras <iveiras@protonmail.com>
2022-09-26 00:04:53 +0200

    Minor changes

--- main.py
+++ main.py
@@ -1,1 +1,62 @@
 # Put the code for your API here.
+from fastapi import FastAPI
+from pydantic import BaseModel
+import os
+import pandas as pd
+
+from starter.ml.model import inference
+
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system("dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
+
+class CensusData(BaseModel):
+    age: int
+    workclass: str
+    fnlgt: int
+    education: str
+    education_num: int
+    marital_status: str
+    occupation: str
+    relationship: str
+    race: str
+    sex: str
+    capital_gain: float
+    capital_loss: float
+    hours_per_week: int
+    native_country: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "age": 52,
+                "workclass": "Self-emp-not-inc",
+                "fnlgt": 209642,
+                "education": "HS-grad",
+                "education_num": 9,
+                "marital_status": "Married-civ-spouse",
+                "occupation": "Exec-managerial",
+                "relationship": "Husband",
+                "race": "White",
+                "sex": "Male",
+                "capital_gain": 0,
+                "capital_loss": 0,
+                "hours_per_week": 45,
+                "native_country": "United-States",
+            }
+        }
+
+
+app = FastAPI()
+
+@app.get("/")
+async def welcome():
+    return {"message": "This is the salary prediction API based on census data."}
+
+@app.post("/predict")
+async def predict_salary(record: CensusData):
+    input_census = pd.read_json(record)
+    salary = inference(input_census)
+    return salary
---
+++ test_api.py
@@ -0,0 +1,27 @@
+from fastapi.testclient import TestClient
+
+# Import our app from main.py.
+from main import app
+
+# Instantiate the testing client with our app.
+client = TestClient(app)
+
+# Write tests using the same syntax as with the requests module.
+def test_api_without_query():
+    r = client.get("/items/12")
+    assert r.status_code == 200
+    assert r.json() == {"fetch": "Fetched 1 of 12"}
+
+def test_api_with_query():
+    r = client.get("/items/12?count=100")
+    assert r.status_code == 200
+    assert r.json() == {"fetch": "Fetched 100 of 12"}
+
+def test_api_wrong_url():
+    r = client.get("/100?count=23")
+    assert r.status_code != 200
+
+if __name__ == "__main__":
+    test_api_with_query()
+    test_api_without_query()
+    test_api_wrong_url()
